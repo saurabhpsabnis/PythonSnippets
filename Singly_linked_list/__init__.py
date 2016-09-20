59de39b2efef89abe26ebd9bdeae151950c83112
Saurabh P Sabnis <saurabhpsabnis@gmail.com>
2016-09-19 22:26:50 -0700

    Commit  : 09/19

--- Singly_linked_list/__init__.py
+++ Singly_linked_list/__init__.py
@@ -26,9 +26,38 @@ class sllNode:
    
 def printSll(node):
     while node:
-        print node.getData()
+        print node.getData(), " ",
         node = node.getNext()
-       
+    print
+
+def reverseSll(node):
+    if(not node):
+        return None
+    t1 = None
+    t2 = node
+    t3 = node.getNext()
+    while (t2):
+        t3 = t2.getNext()
+        t2.setNext(t1)
+        t1 = t2
+        t2 = t3
+    return t1
+
+def insertByIndex(node,data,index):
+    i =0 
+    while i!= index-1:
+        if(node.getNext == None):
+            print "index out of range"
+            return
+        else:
+            node = node.getNext()
+        i +=1
+    nextNode = node.getNext()
+    tempNode = sllNode(data)
+    tempNode.setNext(nextNode)
+    node.setNext(tempNode)
+    
+
 ll = [45,78,11,23,61,89,32,6,112]
 #Creating the SLL from the list
 head = sllNode(ll[0])
@@ -38,6 +67,13 @@ for i in ll[1:]:
     t1.setNext(sll)
     t1 = sll
 printSll(head)
+# reversing the SLL
+head = reverseSll(head)
+printSll(head)
 
+#reversing it back to original 
+head = reverseSll(head)
     
-    
+#inserting an element in the middle 
+insertByIndex(head, 34, 4)
+printSll(head)
--- msc_progs.py
+++ msc_progs.py
@@ -4,7 +4,9 @@ Created on May 10, 2016
 @author: amolsaurabh
 '''
 
-from sorting_algos import sorting
+import os
+import sorting
+
 
 def isPostTraversalOfBST(ll):
     "Determine if given an array is post order of BST "
@@ -35,6 +37,40 @@ def sortedSquareArray(ll):
             result.insert(0,ll[i]*ll[i])
             i +=1
     return result
+
+def parse_syslog():
+    cwd = os.getcwd()
+    os.chdir("/var/log")
+    print os.getcwd()
+    f = open("syslog","r")
+    print f.name, f.mode,
+    f.close()
+    os.chdir(cwd)
+    
+    # f.open 
+    # f.tell 
+    #f.read
+    #f.readlines
+    #f.readline 
+    #f.tell 
+    #f.seek
+    # open file is read binary / write binary  
+    os.chdir("/var/log")
+    with open("syslog") as f:
+        print cwd + "/test.txt"
+        with open(cwd + "/test.txt","w") as g:
+            for line in f:
+                print f.tell()
+                print(line) 
+                g.write(line)
+                
+    with open(cwd + "/test.txt","rw") as f:
+        print f.read()
+    os.chdir(cwd)
+            
+    
+            
+        
         
 if __name__ == '__main__':
     ll = [0, 2, 4, 6, 5, 3, 1, 8, 10, 9, 7 ]
@@ -48,4 +84,4 @@ if __name__ == '__main__':
     print " Case 1: sq sorted array : " + str(sortedSquareArray(temp))
     temp = [5,-86,69,73,11,17,1,74,34,3]
     print " Case 2: sq un sorted array  : " + str(sortedSquareArray(temp))
-    
+  #   parse_syslog()
