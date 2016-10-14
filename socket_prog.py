368e66652c917eaf9d7a0e823b03897e805c488d
Saurabh P Sabnis <saurabhpsabnis@gmail.com>
2016-10-13 23:37:56 -0700

    support  for multi client added

--- socket_prog.py
+++ socket_prog.py
@@ -14,11 +14,11 @@ class clSocket:
         self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
     def clSocket_bind(self,port=34567):
-        self.soc.bind(('',port))
+        self.soc.bind((str(socket.INADDR_ANY),port))
         self.soc.listen(50)
         
-    def clSocket_connect(self,port=34567):
-        self.soc.connect(('',port))
+    def clSocket_connect(self,host = "localhost",port=34567):
+        self.soc.connect((host,port))
 
     def clSocket_recv(self,size=1024):
         x = self.soc.recv(size)
@@ -38,7 +38,7 @@ def handle_client(client, address):
         m = client.recv(1024)
         print m
         client.send(m)
-        if ( m == "exit"):
+        if ( m == "exit" or m == ""):
             print "Client {0} disconnected".format(address)
             break
             
@@ -48,12 +48,14 @@ if __name__ == '__main__':
         raise AssertionError("Please pass port as argument")
     
     while True:
-        print "1.Client"
-        print "2. server"
+        print "1. Client"
+        print "2. server (Can't exit)"
+        print "3. Exit"
         choice = raw_input("Enter your choice")
         if choice == "1":
             mySocket = clSocket()
-            mySocket.clSocket_connect(int(sys.argv[1]))
+            host = raw_input("enter hostname or IP : ")
+            mySocket.clSocket_connect(host,int(sys.argv[1]))
             while True:
                 m = raw_input("Enter your msg ('exit' to exit) :")
                 mySocket.clSocket_send(m)
@@ -67,7 +69,9 @@ if __name__ == '__main__':
             mySocket.clSocket_bind(int(sys.argv[1]))
             while True:
                 client, address = mySocket.soc.accept()
-                thread.start_new_thread(handle_client, (client,address))         
+                thread.start_new_thread(handle_client, (client,address))    
+        elif choice=="3":
+            sys.exit()
         else:
             print "wrong choice"
 
